a0344159465dc29fe2af640578a472cc144b3b4e
aritro4231 <aritro@vt.edu>
2026-01-28 16:32:13 -0500

    playing wth settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://YOUR-FRONTEND.vercel.app",
+    "https://heardrumvercel.app/",
 ]
 
 
